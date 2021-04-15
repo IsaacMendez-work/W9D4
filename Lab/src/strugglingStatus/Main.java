2d73cb82139f52d56926be62161376e6aed6a048
Eyke Mendez <isaacmendez.work+gitbash@gmail.com>
2021-04-15 17:03:01 -0400

    added labwork and screenshots

--- Lab/src/strugglingStatus/Main.java
+++ Lab/src/strugglingStatus/Main.java
@@ -1,8 +1,83 @@
 package strugglingStatus;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+import java.util.*;
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+
+    public static void main(String [] args) {
+/*
+//PROBLEM NUMBER ONE: FIVE RANDOM NUMBERS
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Type out any number on five lines below: ");
+    int myNumber1 = scanner.nextInt();
+    int myNumber2 = scanner.nextInt();
+    int myNumber3 = scanner.nextInt();
+    int myNumber4 = scanner.nextInt();
+    int myNumber5 = scanner.nextInt();
+    List<Integer> arrNumbers = new ArrayList<> ();
+
+    arrNumbers.add(myNumber1);
+    arrNumbers.add(myNumber2);
+    arrNumbers.add(myNumber3);
+    arrNumbers.add(myNumber4);
+    arrNumbers.add(myNumber5);
+//    System.out.println(arrNumbers);
+    int sum = 0, product = 1, max = arrNumbers.get(0), min = arrNumbers.get(0);
+    for(int i = 0; i < 5; i++) {
+        sum += arrNumbers.get(i);
+        product *= arrNumbers.get(i);
+        max = Math.max(max, arrNumbers.get(i));
+        min = Math.min(min, arrNumbers.get(i));
+    }
+// printing the output
+    System.out.println("The sum is " + sum);
+    System.out.println("The product is " + product);
+    System.out.println("The largest number is " + max);
+    System.out.println("The smallest number is " + min);
+}
+*/
+/*
+//PROBLEM NUMBER TWO : CARS
+
+
+        // Creating the Hashmap with Keys: Model, and Value: Make
+        //We defined the type of information by using <String, string> because both the make and model are string values.
+        //We  named our hashmap VEHICLES
+        HashMap<String, String> vehicles = new HashMap <String, String> ();
+        vehicles.put("Cybertruck", "Tesla");
+        vehicles.put("X6", "BMW");
+        vehicles.put("R8", "Audi");
+        vehicles.put("Tundra", "Toyota");
+        vehicles.put("Wrangler", "Jeep");
+
+        // Greets the user, and allows user to put their name and model of car they're looking for.
+        System.out.println("Hi! Welcome to our dealership.\n");
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String yourName = scan1.next();
+        System.out.println("Hello, " + yourName + ". What car model are you looking for?");
+
+        //This is to grab the input from the User from the previous question, the model of the car they're looking for.
+
+        Scanner scanner = new Scanner(System.in);
+        String carChoice = scanner.next();
+
+        //contains.key allows the computer to take the user's input (carChoice) and checks the "hashmap" from above to see if it contains what the user has inputted. It comes out as a boolean (true/false)
+        //the if statement is saying that "IF it is TRUE that the input matches one of the car options above, it will state out, "yes we do have that model, and the make for that model is...."
+
+        if (vehicles.containsKey(carChoice)) {
+            System.out.println("Great! We have that model. ");
+
+            System.out.println("The make for that model is " + (vehicles.get(carChoice))); // the code (vehicles.get(carChoice) uses the user input (the model "a key") and grabs the "make" (value) from the table and displays that (the make "value").
+        }
+        else { //else is saying that if the user input does not match any of the above models, it will print out "sorry we don't carry that model"
+            System.out.println(carChoice + "? Sorry, but we don't carry that model.");
+            System.out.println("Have a nice day!");
+        }
+*/
     }
 }
